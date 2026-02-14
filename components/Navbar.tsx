0254109cef6781fd4376494117fa4e56921501b1
tiwariritika5757-dot <tiwariritika5757@gmail.com>
2026-02-13 17:22:05 -0800

    updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,39 +14,57 @@ export default function Navbar() {
     return () => window.removeEventListener("resize", onResize);
   }, []);
 
+  const closeMenu = () => setOpen(false);
+
   return (
     <header className="ea-header">
       <div className="ea-nav">
-        {/* Left Menu */}
-        <nav className="ea-left">
+        {/* Left Menu (Desktop) */}
+        <nav className="ea-left" aria-label="Primary left">
           <ul>
-            <li><Link href="/">HOME</Link></li>
-            <li><Link href="#about">ABOUT</Link></li>
+            <li>
+              <Link href="/" onClick={closeMenu}>
+                HOME
+              </Link>
+            </li>
+            <li>
+              <Link href="#about" onClick={closeMenu}>
+                ABOUT
+              </Link>
+            </li>
           </ul>
         </nav>
 
-        {/* Center Logo */}
+        {/* Center Logo (Always Center) */}
         <div className="ea-logo">
-          <Link href="/" className="ea-wordmark" onClick={() => setOpen(false)}>
+          <Link href="/" className="ea-wordmark" onClick={closeMenu}>
             SHRUTI&nbsp;CHADHA
           </Link>
         </div>
 
-        {/* Right Menu */}
-        <nav className="ea-right"> 
-          
+        {/* Right Menu (Desktop) */}
+        <nav className="ea-right" aria-label="Primary right">
           <ul>
-            <li><Link href="/Gallery">GALLERY</Link></li>
-            <li><Link href="/contact">CONTACT</Link></li>
+            <li>
+              <Link href="/Gallery" onClick={closeMenu}>
+                GALLERY
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" onClick={closeMenu}>
+                CONTACT
+              </Link>
+            </li>
           </ul>
         </nav>
 
-        {/* Mobile button */}
+        {/* Mobile button (Burger) */}
         <button
           className="ea-burger"
           aria-label="Toggle menu"
           aria-expanded={open}
-          onClick={() => setOpen(v => !v)}
+          aria-controls="ea-mobile-nav"
+          onClick={() => setOpen((v) => !v)}
         >
           <span />
           <span />
@@ -55,11 +73,19 @@ export default function Navbar() {
 
       {/* Mobile dropdown */}
       <div className={`ea-mobile ${open ? "is-open" : ""}`}>
-        <nav className="ea-mobile-nav">
-          <Link href="/" onClick={() => setOpen(false)}>HOME</Link>
-          <Link href="#about" onClick={() => setOpen(false)}>ABOUT</Link>
-          <Link href="/gallery" onClick={() => setOpen(false)}>GALLERY</Link>
-          <Link href="/contact" onClick={() => setOpen(false)}>CONTACT</Link>
+        <nav id="ea-mobile-nav" className="ea-mobile-nav" aria-label="Mobile menu">
+          <Link href="/" onClick={closeMenu}>
+            HOME
+          </Link>
+          <Link href="#about" onClick={closeMenu}>
+            ABOUT
+          </Link>
+          <Link href="/gallery" onClick={closeMenu}>
+            GALLERY
+          </Link>
+          <Link href="/contact" onClick={closeMenu}>
+            CONTACT
+          </Link>
         </nav>
       </div>
     </header>
