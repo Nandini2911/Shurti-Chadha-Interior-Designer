2c007947e363c61aff2041586cf212f65c8a7013
Nandini yadav <nandaniyadav521@gmail.com>
2026-04-21 19:07:14 +0530

    updated

--- app/Gallery/page.tsx
+++ app/Gallery/page.tsx
@@ -1,18 +1,10 @@
-import Navbar from "@/components/Navbar";
-import GalleryRooms from "@/components/galleryrooms";
+
 import Footer from "@/components/Footer";
-import GalleryHero from "@/components/Gallery";
-import GalleryIntro from "@/components/galleryintro";
-import Gallerycta from "@/components/CTASection";
 
 export default function GalleryPage() {
   return (
     <main>
-  
-    <GalleryHero/>
-    <GalleryIntro/>
-    <GalleryRooms/>
-    <Gallerycta/>
+    
       <Footer />
     </main>
   );
